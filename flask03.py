d4fa308c7a198bc8488f3345f7b2768b9a2270c8
Sashank Pyndi <70219849+sashpyndi@users.noreply.github.com>
2022-11-26 11:48:07 -0500

    flask app #3 - added new note route and view

--- flask03.py
+++ flask03.py
@@ -13,18 +13,22 @@ app = Flask(__name__)     # create an app
 @app.route('/')
 @app.route('/index')
 def index():
-    a_user = {'name': 'Sashank', 'email': 'mogli@uncc.edu'}
+    a_user = {'name': 'Sashank', 'email': 'spyndi@uncc.edu'}
     return render_template('index.html', user = a_user)
 @app.route('/notes')
 def get_notes():
     notes = {1:{'title': 'First note', 'text': 'This is my first note', 'date': '10-1-2020'}, 2: {'title': 'Second note', 'text': 
-    'This is my second note','date': '10-2-2020' }}
+    'This is my second note','date': '10-2-2020' } , 3: {'title': 'Third note', 'text': 'This is my third note', 'date': '10-3-2020'}}
     return render_template('notes.html', notes=notes)
 @app.route('/notes/<note_id>')
 def get_note(note_id):
     notes = {1: {'title': 'First note', 'text': 'This is my first note', 'date': '10-1-2020'}, 2: {'title': 'Second note', 'text': 'This is my second note', 'date': '10-2-2020'},
     3: {'title': 'Third note', 'text': 'This is my third note', 'date': '10-3-2020'}}
     return render_template('note.html', note=notes[int(note_id)])
+@app.route('/notes/new')
+def new_note():
+  a_user = {'name': 'Sashank', 'email': 'spyndi@uncc.edu'}
+  return render_template('new.html', user=a_user)
 app.run(host=os.getenv('IP', '127.0.0.1'),port=int(os.getenv('PORT', 5000)),debug=True)
 
 # To see the web page in your web browser, go to the url,
