232a16a8cc9079251f84348d072b487365f727e3
Sashank Pyndi <70219849+sashpyndi@users.noreply.github.com>
2022-11-26 12:30:15 -0500

    flask app #3- captring form data in post reuqest

--- flask03.py
+++ flask03.py
@@ -33,7 +33,8 @@ def new_note():
   print('request method is', request.method)
   a_user = {'name': 'Sashank', 'email': 'spyndi@uncc.edu'}
   if request.method == 'POST':
-    return '<h1> POST method used for this request</h1>'
+    request_data = request.form
+    return f"data: {request_data} !"
   else:
      return render_template('new.html', user=a_user)
 app.run(host=os.getenv('IP', '127.0.0.1'),port=int(os.getenv('PORT', 5000)),debug=True)
